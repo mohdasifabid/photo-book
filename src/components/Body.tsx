8bd1c180d7d4fc4e59003e6bfeaa3c1a3e5477dc
mohdasifdev <mohd.asif@devkraft.in>
2022-11-25 16:28:42 +0530

    :fire: add pagination layout

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,10 +1,19 @@
+import { Pagination } from "@mui/material";
 import { Box } from "@mui/system";
 import { ImageCard } from "./ImageCard";
 
 export const Body = () => {
   return (
-    <Box padding="2rem">
-      <ImageCard img="https://picsum.photos/id/237/200/300" title="some title"/>
+    <Box padding="2rem" display="flex" flexDirection="column" height="100%">
+      <Box flexGrow="1">
+        <ImageCard
+          img="https://picsum.photos/id/237/200/300"
+          title="some title"
+        />
+      </Box>
+      <Box  display="flex" justifyContent="center" flexBasis="5rem" padding="1rem">
+        <Pagination count={10} variant="outlined" shape="rounded" />
+      </Box>
     </Box>
   );
 };
