a09553f050575b00d279ccc48eaa79348d1731d0
mohdasifdev <mohd.asif@devkraft.in>
2022-11-25 14:00:41 +0530

    :hammer: refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css'
-import { Navbar } from './components/Layout'
-
+import { Footer } from './components/Footer'
+import { Navbar } from './components/Navbar'
 function App() {
 
   return (
     <div className="App">
       <Navbar brandName='Photo-Book'/>
+      <Footer />
     </div>
   )
 }
