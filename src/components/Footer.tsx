5d27afebc116cb0d392d5583e905cfdbcd343293
mohdasifdev <mohd.asif@devkraft.in>
2022-11-25 13:14:18 +0530

    :bookmark: add footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,24 @@
+import { Box } from "@mui/material";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import TwitterIcon from "@mui/icons-material/Twitter";
+
+export const Footer = () => {
+  return (
+    <Box
+      display="flex"
+      justifyContent="center"
+      bgcolor={"#F7C59F"}
+      alignItems="center"
+      padding="1rem"
+      color="#1A659E"
+      flexBasis="795px"
+    >
+      <Box display="flex" gap="1rem">
+        <LinkedInIcon fontSize="large" />
+        <GitHubIcon fontSize="large" />
+        <TwitterIcon fontSize="large" />
+      </Box>
+    </Box>
+  );
+};
