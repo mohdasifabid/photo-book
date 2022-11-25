f7762c4f7f178a030c76f19fc92baacfbeadbf31
mohdasifdev <mohd.asif@devkraft.in>
2022-11-25 16:50:32 +0530

    CSS: use flexbox

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -5,13 +5,46 @@ import { ImageCard } from "./ImageCard";
 export const Body = () => {
   return (
     <Box padding="2rem" display="flex" flexDirection="column" height="100%">
-      <Box flexGrow="1">
+      <Box flexGrow="1" display="flex" gap="1rem" flexWrap="wrap">
+        <ImageCard
+          img="https://picsum.photos/id/237/200/300"
+          title="some title"
+        />{" "}
+        <ImageCard
+          img="https://picsum.photos/id/237/200/300"
+          title="some title"
+        />{" "}
+        <ImageCard
+          img="https://picsum.photos/id/237/200/300"
+          title="some title"
+        />{" "}
+        <ImageCard
+          img="https://picsum.photos/id/237/200/300"
+          title="some title"
+        />{" "}
+        <ImageCard
+          img="https://picsum.photos/id/237/200/300"
+          title="some title"
+        />{" "}
+        <ImageCard
+          img="https://picsum.photos/id/237/200/300"
+          title="some title"
+        />{" "}
+        <ImageCard
+          img="https://picsum.photos/id/237/200/300"
+          title="some title"
+        />{" "}
         <ImageCard
           img="https://picsum.photos/id/237/200/300"
           title="some title"
         />
       </Box>
-      <Box  display="flex" justifyContent="center" flexBasis="5rem" padding="1rem">
+      <Box
+        display="flex"
+        justifyContent="center"
+        flexBasis="5rem"
+        padding="1rem"
+      >
         <Pagination count={10} variant="outlined" shape="rounded" />
       </Box>
     </Box>
