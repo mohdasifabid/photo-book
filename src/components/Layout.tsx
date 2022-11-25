1b9db90fb52cf7d4ab97a78b1b9fa19f2159b2c5
mohdasifdev <mohd.asif@devkraft.in>
2022-11-25 14:25:15 +0530

    :bookmark: add layout

---
+++ src/components/Layout.tsx
@@ -0,0 +1,16 @@
+import { Box } from "@mui/system";
+import { Footer } from "./Footer";
+import { Navbar } from "./Navbar";
+
+type childrenType = {
+  children: JSX.Element;
+};
+export const Layout = ({ children }: childrenType) => {
+  return (
+    <Box display="flex" flexDirection="column" height="98vh" overflow="hidden">
+      <Navbar brandName="Photo-Book" />
+      <Box flexGrow="1">{children}</Box>
+      <Footer />
+    </Box>
+  );
+};
